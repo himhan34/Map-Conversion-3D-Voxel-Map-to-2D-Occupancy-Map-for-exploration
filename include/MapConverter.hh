291d225c2b19ad36afc0b93eb74aa59c034c0ff3
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-11-23 22:49:07 +0900

    Update MapConverter.hh

--- include/MapConverter.hh
+++ include/MapConverter.hh
@@ -1,26 +1,29 @@
-#pragma once
+#pragma once  // 헤더 파일의 중복 포함 방지
 
-#include "HeightRangeMap.hh"
-#include "Map2D.hh"
-#include "datatypes.hh"
+#include "HeightRangeMap.hh"  // HeightRangeMap 관련 헤더 포함
+#include "Map2D.hh"  // Map2D 관련 헤더 포함
+#include "datatypes.hh"  // 사용자 정의 데이터 타입 헤더 포함
 
-using namespace std;
+using namespace std;  // 표준 네임스페이스 사용
 
-class MapConverter {
+class MapConverter {  // 맵 변환기를 정의하는 클래스
 public:
-  Map2D map;
+  Map2D map;  // 2D 맵 객체
+  // 생성자: 해상도, 경사 추정 크기, 최소 Z 값, 최소 점유 수준 초기화
   MapConverter(double resolution, int slopeEstimationSize, double minimumZ,
                int minimumOccupancy);
-  ~MapConverter();
-  // update global map in region of the voxels given
+  ~MapConverter();  // 소멸자
+
+  // 주어진 복셀 정보로 글로벌 맵을 업데이트
   void updateMap(vector<voxel> vMap, vector<double> minMax);
-  // get the min max values in x and y for given voxel list agins map frame.
-  // Returns vector with <minX, maxX, minY, maxY, minZ, maxZ>
+
+  // 주어진 복셀 리스트에 대해 맵 프레임 기준의 x, y 최소/최대값을 계산
+  // 반환값: <minX, maxX, minY, maxY, minZ, maxZ> 형태의 벡터
   vector<double> minMaxVoxel(vector<voxel> list);
 
 private:
-  int slopeEstimationSize;
-  double resolution;
-  int minOcc;
-  double minZ;
+  int slopeEstimationSize;  // 경사 추정을 위한 영역 크기
+  double resolution;  // 맵의 해상도
+  int minOcc;  // 최소 점유 수준
+  double minZ;  // 맵의 최소 Z 값
 };
