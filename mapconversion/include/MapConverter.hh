3c10da15916f56e9fb82cd7d9ac8f66026458634
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2024-11-23 21:41:32 +0900

    Update MapConverter.hh

--- mapconversion/include/MapConverter.hh
+++ mapconversion/include/MapConverter.hh
@@ -1,25 +1,28 @@
-#pragma once
+#pragma once // 헤더 파일의 중복 포함을 방지하기 위해 사용
 
-#include "Map2D.hh"
-#include "datatypes.hh"
+#include "Map2D.hh" // Map2D 클래스의 정의를 포함하기 위해 Map2D.hh 파일을 포함
+#include "datatypes.hh" // voxel 및 관련 데이터 타입 정의를 포함하기 위해 datatypes.hh 파일을 포함
 
-using namespace std;
+using namespace std; // 표준 라이브러리의 네임스페이스(std)를 전역에서 사용하도록 설정
 
+// MapConverter 클래스 정의
 class MapConverter {
 public:
-  Map2D map;
+  Map2D map; // 2D 맵 데이터를 저장하는 Map2D 클래스 객체
   MapConverter(double resolution, int slopeEstimationSize, double minimumZ,
-               int minimumOccupancy);
-  ~MapConverter();
-  // update global map in region of the voxels given
+               int minimumOccupancy); // 클래스의 생성자로, 매개변수를 통해 초기화
+  ~MapConverter(); // 클래스 소멸자, 리소스 해제를 위해 사용
+
+  // 주어진 voxel 리스트를 기반으로 지정된 영역 내에서 전역 맵을 업데이트하는 함수
   void updateMap(vector<voxel> vMap, vector<double> minMax);
-  // get the min max values in x and y for given voxel list agins map frame.
-  // Returns vector with <minX, maxX, minY, maxY, minZ, maxZ>
+
+  // 주어진 voxel 리스트를 맵 프레임 기준으로 변환하여 x, y, z 축의 최소/최대 값을 반환하는 함수
+  // <minX, maxX, minY, maxY, minZ, maxZ> 형태의 벡터를 반환
   vector<double> minMaxVoxel(vector<voxel> list);
 
 private:
-  int slopeEstimationSize;
-  double resolution;
-  int minOcc;
-  double minZ;
+  int slopeEstimationSize; // 경사 추정을 위한 윈도우 크기
+  double resolution; // 맵의 해상도
+  int minOcc; // 점유를 최소로 간주하는 점유 값
+  double minZ; // Z 축의 최소값 기준
 };
